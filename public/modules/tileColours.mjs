dbb75f0eac69381e2c5392be1b0e3ee28ec414ec
Edison Du <duedison794@gmail.com>
2023-08-26 23:48:49 -0400

    Tile Colour

--- public/modules/tileColours.mjs
+++ public/modules/tileColours.mjs
@@ -2,17 +2,21 @@ class TileColour {
     strokeColour;
     fillColour;
     selectColour;
-    constructor(strokeColour, fillColour, selectColour) {
+    leftShadow;
+    rightShadow;
+    constructor(strokeColour, fillColour, selectColour, leftShadow, rightShadow) {
         this.strokeColour = strokeColour;
         this.fillColour = fillColour;
         this.selectColour = selectColour;
+        this.leftShadow = leftShadow;
+        this.rightShadow = rightShadow;
     }
 }
 
 const tileColours = [
     // gray
     // blue
-    new TileColour('blue', '#74c2ff', '#aadcff'),
+    new TileColour('blue', '#74c2ff', '#aadcff', "#478eff", "#5e9af4"),
     // red
     // etc
 ]
